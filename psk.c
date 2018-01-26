58c7d927750ff2f34b3cc37bbcb958103ee9a403
gopinaths922515114036 <35653143+gopinaths922515114036@users.noreply.github.com>
2018-01-26 10:58:18 +0000

    Create psk.c

---
+++ psk.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("input hours");
+  scanf("%d",&a);
+  printf("input minutes");
+  scanf("%d",b);
+  c=a*60+b;
+  printf("OUTPUT",&C);
+  return 0;
+  }
+
