0e11053e4ceceaa1c556fe3120915fa7ab5b63f5
gopinaths922515114036 <35653143+gopinaths922515114036@users.noreply.github.com>
2018-02-08 16:07:02 +0530

    Create addoreven.c

---
+++ addoreven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+ 
+    if(num % 4 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+
+    getch();
+}
