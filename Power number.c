c3487598597796598364233bd9f6c2df9b1483af
gopinaths922515114036 <35653143+gopinaths922515114036@users.noreply.github.com>
2018-02-27 16:58:13 +0530

    Create Power number.c

---
+++ Power number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,g,m;
+clrscr();
+printf("Enter the value \n");
+scanf("%d",&a);
+printf(" Enter the value \n");
+scanf("%d",&g);
+m=pow(a,g);
+printf("%d \n",m);
+getch();
+return 0;
+}
