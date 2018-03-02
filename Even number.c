f5b89f54e4e363141960278d4ac5fa6adf9330be
gopinaths922515114036 <35653143+gopinaths922515114036@users.noreply.github.com>
2018-03-02 14:36:36 +0530

    Create Even number.c

---
+++ Even number.c
@@ -0,0 +1,17 @@
+#includde<stio.h>
+void main()
+{
+
+int h,d,i=0;
+ printf("enter the  starting number    : ",h);
+ scanf("%d",&h);
+ printf("enter the ending number    :  ",d);
+ scanf("%d",&d);
+for(i=h;i<=d;i++)
+{
+if(i%2!=0);
+else
+printf("%d\n",i);
+}
+    
+}
