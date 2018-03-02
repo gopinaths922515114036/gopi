019da539140f93220e0da6eeee0004e3d8bc60b9
gopinaths922515114036 <35653143+gopinaths922515114036@users.noreply.github.com>
2018-03-02 14:15:21 +0530

    Create Prime or not.c

---
+++ Prime or not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int g,k;
+  printf("Enter the value   :  ");
+  scanf("%d",&g);
+if(g%2==0)
+ {
+  printf("number is not prime");
+ }
+else
+ {
+  printf("number is prime");
+ } 
+}
