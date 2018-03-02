518e1dd6edb7cec099a97797e6c6e81517c47e00
gopinaths922515114036 <35653143+gopinaths922515114036@users.noreply.github.com>
2018-03-02 14:26:25 +0530

    Create Odd number.c

---
+++ Odd number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+
+int h,p,i=0;
+ printf("enter the  starting number    : ",h);
+ scanf("%d",&h);
+ printf("enter the ending number    :  ",p);
+ scanf("%d",&p);
+for(i=h;i<=p;i++)
+{
+if(i%2==0);
+else
+printf("%d\n",i);
+}
+    
+}
+
