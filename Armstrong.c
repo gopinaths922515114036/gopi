2719d161bcdb564a01548b24554b3046982ee63e
gopinaths922515114036 <35653143+gopinaths922515114036@users.noreply.github.com>
2018-03-08 16:03:23 +0530

    Create Armstrong.c

---
+++ Armstrong.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{
+int g ;
+printf("enter the n values  : ");
+scanf("%d",&g);
+for(g=0;g<1;g++)
+{
+if(g*2==0)
+{
+printf("this is armstrong number");
+}
+else
+{
+printf("not armstrong number");
+}
+}
+}
